c4f8aeb9822a9eb5dd1ba2d8948adb39a0d17520
gianluur <gianluca.rssu@gmail.com>
2024-11-07 23:54:23 +0100

    New parser: Added variables, functions, assignments, expressions (with cast)

--- src/frontend/new_parser.hpp
+++ src/frontend/new_parser.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 #include <unordered_map>  
 #include <unordered_set>
-#include <optional>
 #include <stack>
+#include <optional>
 
 #include "../includes/token.hpp"
 #include "../includes/new_ast.hpp"
@@ -16,6 +17,7 @@
 using std::cout, std::endl;
 using std::stoi, std::stod;
 using std::vector, std::unordered_map, std::unordered_set, std::stack;
+using std::optional;
 
 #undef NULL
 
@@ -104,6 +106,12 @@ private:
       case TokenType::VAR:
       case TokenType::CONST:
         return parseVariable();
+
+      case TokenType::FUNC:
+        return parseFunction();
+
+      case TokenType::IDENTIFIER:
+        return parseIdentifier({}, false);
       
       default:
         error("Token Not handled yet: " + token.lexemes, m_line);
@@ -128,8 +136,43 @@ private:
     return m_tokens[index];
   }
 
+  bool isAtEnd() const {
+    return index >= m_tokens.size();
+  }
+
   bool isNextTokenType(enum TokenType type){
     return nextToken().type == type;
+  }
+
+  int getPrecedence(const TokenType type) const {
+    switch (type) {
+      case TokenType::MULTIPLICATION: 
+      case TokenType::DIVISION:
+      case TokenType::MODULUS:
+        return 3;
+
+      case TokenType::ADDITION: 
+      case TokenType::SUBTRACTION:
+        return 2;
+
+      case TokenType::GREATER: 
+      case TokenType::GREATER_EQUAL:
+      case TokenType::LESS: 
+      case TokenType::LESS_EQUAL:
+      case TokenType::EQUALS:
+        return 1;
+
+      case TokenType::AND:
+      case TokenType::OR:
+        return 0;
+
+      default:
+        return -1;
+    }
+  }
+
+  bool isLeftAssociative(const TokenType type) const {
+    return type != TokenType::NOT; // Unary NOT is right associative
   }
 
   bool isAssigmentOperator(const Token& token){
@@ -148,6 +191,10 @@ private:
     return mathOperatorSet.find(token.type) != mathOperatorSet.end();
   }
 
+  bool isOperator(const Token& token){
+    return isMathOperator(token) || isBooleanOperator(token) || isComparisonOperator(token);
+  }
+
   bool isLiteral(const Token& token) const {
     return literalsMap.find(token.type) != literalsMap.end();
   }
@@ -157,13 +204,12 @@ private:
   }
 
   bool isValidExpression(const Token& token) {
-    return (index >= m_tokens.size()) && 
-          (isMathOperator(token) || isBooleanOperator(token) || isComparisonOperator(token) || 
-          isLiteral(token) || isType(token) || token.type == TokenType::IDENTIFIER || 
-          token.type == TokenType::LPAREN || token.type == TokenType::RPAREN);
+    return !(index >= m_tokens.size()) && (
+          isLiteral(token) || isType(token) || isOperator(token) || 
+          token.type == TokenType::IDENTIFIER || token.type == TokenType::LPAREN || token.type == TokenType::RPAREN);
   }
 
-  unique_ptr<Variable> parseVariable(){
+  unique_ptr<Variable> parseVariable(const bool isMember = false){
     const Token& keyword = consumeToken(); // consumes the var/const token
 
     if (!isType(nextToken()))
@@ -178,17 +224,252 @@ private:
       consumeToken(); // consumes ';'
       return make_unique<Variable>(keyword, std::move(type), std::move(identifier));
     }
-    // else if (isNextTokenType(TokenType::ASSIGNMENT)){
-    //   consumeToken(); // consumes '='
+    else if (isNextTokenType(TokenType::ASSIGNMENT) && !isMember){
+      consumeToken(); // consumes '='
 
-    //   if (!isValidExpression(nextToken()))
-    //     error("In variable declaration was expected an appropriate value after assigment operator: " + keyword + " " + type + " " + identifier->toString(), m_line);
+      if (!isValidExpression(nextToken()))
+        error("In variable declaration was expected an appropriate value after assigment operator: " + keyword.lexemes + " " + type->toString() + " " + identifier->toString(), m_line);
+      unique_ptr<Expression> value = parseExpression();
+      
+      if (!isNextTokenType(TokenType::SEMICOLON))
+        error("In variable declaration was expected a semicolon", m_line);
+      consumeToken();
 
-    // }
-    error("Variable stuff not handled yet\n");
+      return make_unique<Variable>(keyword, std::move(type), std::move(identifier), std::move(value));
+      
+    }
+    else
+      error(std::string((isMember) ? "In struct" : "In variable") + "declaration was expected a semicolon", m_line);
   }
 
+  unique_ptr<Function> parseFunction(){
+    consumeToken();
 
+    if (!isType(nextToken()))
+      error("In function declaration was expected a type after func keyword", m_line);
+    unique_ptr<Type> type = make_unique<Type>(consumeToken());
 
-};
- 
+    if (!isNextTokenType(TokenType::IDENTIFIER))
+      error("In function declaration was expected a identifier after type: func " + type->toString(), m_line);
+    unique_ptr<Identifier> identifier = make_unique<Identifier>(consumeToken());
+
+    if (!isNextTokenType(TokenType::LPAREN))
+      error("In function declaration was expected a opening parenthesis after identifier: func " + type->toString()  + identifier->toString(), m_line);
+    consumeToken();
+
+    vector<unique_ptr<Parameter>> parameters = {};
+    while(!isNextTokenType(TokenType::RPAREN)){
+      parameters.push_back(parseParameter());
+
+      if (isNextTokenType(TokenType::COMMA)){
+        consumeToken();
+        if (isNextTokenType(TokenType::RPAREN))
+          error("In function declaration, when passing parameters, there's a comma but no parameter after it, remove the comma or pass the parameter: func " + type->toString()  + identifier->toString());
+      }
+    }
+    consumeToken(); //consumes the ')'
+    unique_ptr<Body> body = parseBody();
+    
+    return make_unique<Function>(std::move(type), std::move(identifier), std::move(parameters), std::move(body));
+  }
+
+  unique_ptr<Parameter> parseParameter(){
+    if (!isType(nextToken()))
+      error("In function declaration was expected a type for the parameter", m_line);
+    unique_ptr<Type> type = make_unique<Type>(consumeToken());
+
+    if (!isNextTokenType(TokenType::IDENTIFIER))
+      error("In function declaration was expected a identifer after type for a parameter:" + type->toString(), m_line);
+    unique_ptr<Identifier> identifier = make_unique<Identifier>(consumeToken());
+
+    return make_unique<Parameter>(std::move(type), std::move(identifier));
+  }
+
+  unique_ptr<Body> parseBody() {
+    if (!isNextTokenType(TokenType::LCURLY))
+      error("Expected open curly bracket for the body", m_line);
+    consumeToken();
+
+    vector<unique_ptr<ASTNode>> statements = {};
+    while(!isNextTokenType(TokenType::RCURLY)){
+      unique_ptr<ASTNode> node = getASTNode();
+      statements.push_back(std::move(node));
+    }
+    consumeToken(); // consumes the '}'
+
+    return make_unique<Body>(std::move(statements));
+  }
+
+  unique_ptr<FunctionCall> parseFunctionCall(const Token& token){
+    consumeToken();
+        
+    unique_ptr<Identifier> identifier = make_unique<Identifier>(token);
+    vector<unique_ptr<Expression>> arguments;
+
+    if (isNextTokenType(TokenType::RPAREN))
+      consumeToken();
+    else {
+      do {
+        arguments.push_back(make_unique<Expression>(parseExpression(true)));
+
+        if (isNextTokenType(TokenType::COMMA)){
+          consumeToken();
+          if (isNextTokenType(TokenType::RPAREN))
+            error("In function call was expected another parameter after comma", m_line);
+        }
+      } while (!isNextTokenType(TokenType::RPAREN));
+
+      if (!isNextTokenType(TokenType::RPAREN))
+        error("In function call was expected a closing parenthesis after the arguments", m_line);
+      consumeToken();
+    }
+
+    if (!isValidExpression(nextToken()) && !isNextTokenType(TokenType::SEMICOLON))
+      error("In function call was expected a semicolon", m_line);
+    return make_unique<FunctionCall>(std::move(identifier), std::move(arguments));
+  }
+
+  unique_ptr<ASTNode> parseIdentifier(optional<Token> token, const bool isInsideExpression = false) {
+    if (!token.has_value())
+      token = consumeToken();
+    
+    if (isInsideExpression){
+      if (isNextTokenType(TokenType::LPAREN))
+        return parseFunctionCall(token.value());
+      else 
+        return make_unique<Identifier>(token.value());
+    }
+    else
+      return parseAssignmentOperator(token.value());
+  }
+
+  unique_ptr<AssignmentOperator> parseAssignmentOperator(const Token& token) {
+    unique_ptr<Identifier> identifier = make_unique<Identifier>(token);
+    
+    if (!isAssigmentOperator(nextToken()))
+      error("In assignment operator was expected the operator after the identifier", m_line);
+    unique_ptr<Operator> op = make_unique<Operator>(consumeToken());
+
+    if (!isValidExpression(nextToken()))
+      error("In assignment operator was expected a valid expression after the operator", m_line);
+    unique_ptr<Expression> value = make_unique<Expression>(parseExpression());
+
+    if (!isNextTokenType(TokenType::SEMICOLON))
+      error("In assignment operator was expected a semicolon after the value", m_line);
+    consumeToken();
+
+    return make_unique<AssignmentOperator>(std::move(identifier), std::move(op), std::move(value));
+  }
+
+  unique_ptr<Cast> parseCast(const Token& token){
+    unique_ptr<Type> type = make_unique<Type>(token);
+    if (token.type == TokenType::NULL)
+      error("Casting any type to type 'null' is not allowed", m_line);
+
+    if (!isNextTokenType(TokenType::LPAREN))
+      error("In cast expression was expected an opening parenthesis", m_line);
+    consumeToken();
+
+    unique_ptr<Expression> expression = parseExpression(true);
+
+    if (!isNextTokenType(TokenType::RPAREN))
+      error("In cast expression was expected a closing parenthesis", m_line);
+    consumeToken();
+
+    return make_unique<Cast>(std::move(type), std::move(expression));
+  }
+
+  unique_ptr<Expression> parseExpression(const bool isArgument = false) {
+    stack<unique_ptr<Operator>> operators;
+    stack<unique_ptr<ASTNode>> nodes;
+    int parenCount = 0;
+
+    while (!isAtEnd()) {
+      if ((!isArgument && isNextTokenType(TokenType::SEMICOLON)) || 
+          (isArgument && parenCount == 0 && (isNextTokenType(TokenType::COMMA) || isNextTokenType(TokenType::RPAREN)))) 
+        break;
+
+      const Token& token = nextToken();
+
+      switch (token.type) {
+        case TokenType::IDENTIFIER:
+          consumeToken();
+          nodes.push(parseIdentifier(token, true));
+          break;
+
+        case TokenType::LPAREN:
+          consumeToken();
+          operators.push(make_unique<Operator>(token));
+          parenCount++;
+          break;
+
+        case TokenType::RPAREN:
+          consumeToken();
+          while (!operators.empty() && operators.top()->getOperator() != TokenType::LPAREN) {
+            handleOperator(operators, nodes);
+          }
+          if (!operators.empty() && operators.top()->getOperator() == TokenType::LPAREN) {
+            operators.pop(); // Pop '('
+            parenCount--;
+          } else if (parenCount > 0) {
+            error("Mismatched parentheses in expression", m_line);
+          }
+          break;
+
+        default:
+          if (isLiteral(token)) {
+            consumeToken();
+            nodes.push(make_unique<Literal>(token));
+          } 
+          else if (isOperator(token)) {
+            consumeToken();
+            int currentPrecedence = getPrecedence(token.type);
+            bool leftAssociative = isLeftAssociative(token.type);
+
+            while (!operators.empty() &&
+                operators.top()->getOperator() != TokenType::LPAREN &&
+                ((leftAssociative && currentPrecedence <= getPrecedence(operators.top()->getOperator())) ||
+                (!leftAssociative && currentPrecedence < getPrecedence(operators.top()->getOperator())))) {
+              handleOperator(operators, nodes);
+            }
+            operators.push(make_unique<Operator>(token));
+          }
+          else if (isType(token)){
+            consumeToken();
+            nodes.push(parseCast(token));
+          } 
+          else 
+            error("Unknown token in expression: " + token.lexemes, m_line);
+      }
+    }
+
+    if (parenCount > 0)
+      error("Unmatched opening parenthesis in expression", m_line);
+
+    while (!operators.empty()) {
+      if (operators.top()->getOperator() == TokenType::LPAREN) 
+        error("Unmatched opening parenthesis in expression", m_line);
+      handleOperator(operators, nodes);
+    }
+
+    return nodes.empty() ? nullptr : make_unique<Expression>(std::move(nodes.top()));
+  }
+
+  void handleOperator(stack<unique_ptr<Operator>>& operators, stack<unique_ptr<ASTNode>>& nodes) {
+    unique_ptr<Operator> op = std::move(operators.top());
+    operators.pop();
+
+    if (op->getOperator() == TokenType::NOT) {
+      std::unique_ptr<ASTNode> right = std::move(nodes.top());
+      nodes.pop();
+      nodes.push(std::make_unique<UnaryOperator>(std::move(op), std::move(right)));
+    } 
+    else {
+      std::unique_ptr<ASTNode> right = std::move(nodes.top());
+      nodes.pop();
+      std::unique_ptr<ASTNode> left = std::move(nodes.top());
+      nodes.pop();
+      nodes.push(std::make_unique<BinaryOperator>(std::move(left), std::move(op), std::move(right)));
+    }
+  }
+};  
--- src/includes/new_ast.hpp
+++ src/includes/new_ast.hpp
@@ -16,6 +16,67 @@ public:
   virtual void print(int indentation_level = 0) const = 0;
 };
 
+class Operator: public ASTNode {
+public:
+  Operator(const Token& token): op(token.type), m_str(token.lexemes) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Operator: " << m_str << '\n';
+  }
+
+  TokenType getOperator() const {
+    return op;
+  }
+
+private:
+  const TokenType op;
+  const string m_str;
+};
+
+class BinaryOperator: public ASTNode {
+public:
+  BinaryOperator(unique_ptr<ASTNode> left, unique_ptr<Operator> op, unique_ptr<ASTNode> right):
+    m_left(std::move(left)), m_op(std::move(op)), m_right(std::move(right)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Binary Operator{\n";
+    m_left->print(indentation_level + 2);
+    m_op->print(indentation_level + 2);
+    m_right->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  TokenType getOperator() const {
+    return m_op->getOperator();
+  }
+
+private:
+  unique_ptr<ASTNode> m_left;
+  unique_ptr<Operator> m_op;
+  unique_ptr<ASTNode> m_right;
+};
+
+class UnaryOperator: public ASTNode {
+public:
+  UnaryOperator(unique_ptr<Operator> op, unique_ptr<ASTNode> right):
+    m_op(std::move(op)), m_right(std::move(right)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Unary Operator{";
+    m_op->print(indentation_level + 2);
+    m_right->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  TokenType getOperator() const {
+    return m_op->getOperator();
+  }
+
+private:
+  unique_ptr<Operator> m_op;
+  unique_ptr<ASTNode> m_right;
+};
+
 class Type : public ASTNode {
 public:
   Type(const Token& token) : m_type(token.type), m_str(token.lexemes) {}
@@ -99,6 +160,54 @@ private:
   const string m_str;
 };
 
+class Expression: public ASTNode {
+public:
+  Expression(unique_ptr<ASTNode> start):
+    m_start(std::move(start)) {}
+
+  Expression():
+    m_start(make_unique<Literal>()) {}
+    
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Expression {\n";
+    m_start->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  ASTNode* getExpression() const {
+    return m_start.get();
+  }
+
+private:
+  unique_ptr<ASTNode> m_start;
+};
+
+class Cast: public ASTNode {
+public:
+  Cast(unique_ptr<Type> type, unique_ptr<Expression> expression):
+    m_type(std::move(type)), m_expression(std::move(expression)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Cast {\n";
+    m_type->print(indentation_level + 2);
+    m_expression->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  ASTNode* getExpression() const {
+    return m_expression->getExpression();
+  }
+
+  TokenType getType() const {
+    return m_type->getType();
+  }
+
+private:
+  unique_ptr<Type> m_type;
+  unique_ptr<Expression> m_expression;
+};
+
 class Identifier: public ASTNode {
 public:
   Identifier(const Token& token): m_str(token.lexemes) {}
@@ -115,17 +224,36 @@ private:
   const string m_str;
 };
 
-class Expression: public ASTNode {
+class AssignmentOperator: public ASTNode {
+public:
+  AssignmentOperator(unique_ptr<Identifier> identifier, unique_ptr<Operator> op, unique_ptr<Expression> value):
+    m_identifier(std::move(identifier)), m_op(std::move(op)), m_value(std::move(value)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << '\n' << setw(indentation_level) << " " << "Assignment Operator {\n";
+    m_identifier->print(indentation_level + 2);
+    m_op->print(indentation_level + 2);
+    m_value->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+private:
+  unique_ptr<Identifier> m_identifier;
+   unique_ptr<Operator> m_op;
+   unique_ptr<Expression> m_value;
 
 };
 
 class Variable: public ASTNode {
 public:
   Variable(const Token& keyword, unique_ptr<Type> type, unique_ptr<Identifier> identifier):
-    m_keyword(keyword), m_type(std::move(type)), m_identifier(std::move(identifier)), m_value(make_unique<Literal>()) {}
+    m_keyword(keyword), m_type(std::move(type)), m_identifier(std::move(identifier)), m_value(make_unique<Expression>()) {}
+    
+  Variable(const Token& keyword, unique_ptr<Type> type, unique_ptr<Identifier> identifier, unique_ptr<Expression> value):
+    m_keyword(keyword), m_type(std::move(type)), m_identifier(std::move(identifier)), m_value(std::move(value)) {}
 
   void print(int indentation_level = 0) const override {
-    cout << '\n' << setw(indentation_level) << " " << "Variable {" << '\n';
+    cout << '\n' << setw(indentation_level) << " " << "Variable {\n";
     cout << setw(indentation_level + 2) << " " << "keyword: " << m_keyword.lexemes << '\n';
     m_type->print(indentation_level + 2);
     m_identifier->print(indentation_level + 2);
@@ -137,5 +265,108 @@ private:
   const Token& m_keyword;
   unique_ptr<Type> m_type;
   unique_ptr<Identifier> m_identifier;
-  unique_ptr<Literal> m_value;
+  unique_ptr<Expression> m_value;
 };
+
+class FunctionCall: public ASTNode {
+public:
+  FunctionCall(unique_ptr<Identifier> identifier, vector<unique_ptr<Expression>> arguments):
+    m_identifier(std::move(identifier)), m_arguments(std::move(arguments)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << setw(indentation_level) << " " << "Function Call{\n";
+    m_identifier->print(indentation_level + 2);    
+    for(const auto& argument: m_arguments){
+      argument->print(indentation_level + 2);
+    }
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+private:
+  unique_ptr<Identifier> m_identifier;
+  vector<unique_ptr<Expression>> m_arguments;
+
+};
+
+class Parameter: public ASTNode {
+public:
+  Parameter(unique_ptr<Type> type, unique_ptr<Identifier> name):
+    m_type(std::move(type)), m_identifier(std::move(name)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << '\n' << setw(indentation_level) << " " << "Parameter {\n";
+    m_type->print(indentation_level + 2);
+    m_identifier->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  TokenType getType() const {
+    return m_type->getType();
+  }
+
+  string getIdentifier() const {
+    return m_identifier->toString();
+  }
+
+private:
+  unique_ptr<Type> m_type;
+  unique_ptr<Identifier> m_identifier;
+};
+
+class Body: public ASTNode {
+public:
+  Body(vector<unique_ptr<ASTNode>> statements):
+  m_statements(std::move(statements)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << '\n' << setw(indentation_level) << " " << "Body {";
+    for(const auto& statement: m_statements){
+      statement->print(indentation_level + 2);
+    }
+    cout << setw(indentation_level) << " " << "}\n";
+  } 
+
+private:
+  vector<unique_ptr<ASTNode>> m_statements;
+};
+
+class Function: public ASTNode {
+public:
+  Function(unique_ptr<Type> type, unique_ptr<Identifier> identifier, vector<unique_ptr<Parameter>> parameters, unique_ptr<Body> body):
+    m_type(std::move(type)), m_identifier(std::move(identifier)), m_parameters(std::move(parameters)), m_body(std::move(body)) {}
+
+  void print(int indentation_level = 0) const override {
+    cout << '\n' << setw(indentation_level) << " " << "Function {\n";
+    m_type->print(indentation_level + 2);
+    m_identifier->print(indentation_level + 2);
+    for(const auto& parameter: m_parameters){
+      parameter->print(indentation_level + 2);
+    }
+    m_body->print(indentation_level + 2);
+    cout << setw(indentation_level) << " " << "}\n";
+  }
+
+  TokenType getType() const {
+    return m_type->getType();
+  }
+
+  string getIdentifier() const {
+    return m_identifier->toString();
+  }
+
+  vector<Parameter*> getParameter() const {
+    vector<Parameter*> parameters;
+    for(const auto& current: m_parameters){
+      parameters.push_back(current.get());
+    }
+    return parameters;
+  }
+
+private:
+  unique_ptr<Type> m_type;
+  unique_ptr<Identifier> m_identifier;
+  vector<unique_ptr<Parameter>> m_parameters;
+  unique_ptr<Body> m_body;
+};
+
+
